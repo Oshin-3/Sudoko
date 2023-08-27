f29c5adfb07fcc2a7c035832fd9abb5d81797525
Oshin-3 <oshin.gawande3@gmail.com>
2023-08-27 22:56:32 +0530

    basic code completed

--- sudoko.js
+++ sudoko.js
@@ -34,6 +34,7 @@ window.onload = function()
 
 function setGame()
 {
+    //appending number to select from 
     for (let i = 1; i <= 9; i++) {
         //<div id = "1" class = "number">1</div>
         let num = document.createElement("div");
@@ -43,6 +44,44 @@ function setGame()
         num.classList.add("number");
         document.getElementById("digits").appendChild(num);
     }
+
+    //appending board
+    for (let i = 0; i < 9; i++)
+    {
+        for (let j = 0; j < 9; j++) {
+            let tile = document.createElement("div");
+            tile.id = i.toString() + '-' + j.toString();
+            if (board[i][j] != '-')
+            {
+                tile.innerText = board[i][j];
+                tile.classList.add("board-color");
+            } 
+            else
+            {
+                tile.classList.add("solution-board-color");
+            }
+            if (i == 2 || i == 5)
+            {
+                tile.classList.add("box-border-left");
+            }
+            else if (i != 8)
+            {
+                tile.classList.add("bottom-border");
+            }
+
+            if (j == 2 || j == 5)
+            {
+                tile.classList.add("box-border-right");
+            }
+            else if (j != 8)
+            {
+                tile.classList.add("right-border");
+            }
+            tile.classList.add("tiles");   
+            tile.addEventListener("click", solutionNumber);        
+            document.getElementById("board").appendChild(tile);            
+        }
+    }   
 }
 
 function selectNumber()
@@ -54,3 +93,29 @@ function selectNumber()
     numSeleted = this;
     numSeleted.classList.add("number-selected");
 }
+
+function solutionNumber()
+{
+    if (numSeleted)
+    {
+        let coords = this.id.split("-"); //["0", "0"]
+        let r = parseInt(coords[0]);
+        let c = parseInt(coords[1]);
+
+        if (this.innerText != "" && solution[r][c] != numSeleted.id)
+        {
+            this.innerText = numSeleted.id;
+        }
+
+        this.innerText = numSeleted.id;
+        if (solution[r][c] != numSeleted.id)
+        {
+            errors += 1;
+            document.getElementById("error-count").innerText = errors;
+        }
+
+    }
+    
+}
+
+
